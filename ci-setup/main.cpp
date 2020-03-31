bc136d40a1c4fe72db6f05ae6472fbc4eb033f27
SGCube <sgcube@yandex.ru>
2020-03-31 20:20:32 +0300

    ci-setup: memcheck test

--- ci-setup/main.cpp
+++ ci-setup/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
-int main(int argc,      char **argv)
-{ std::cout << "Hello, home!"     << std::endl;
-    return 0;}
+int main(int argc, char **argv)
+{
+    std::cout << "Hello, home!" << std::endl;
+    return 0;
+}
