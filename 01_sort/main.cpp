e66f2d60295d2f9251aeb6f084305086724370f4
SGCube <sgcube@yandex.ru>
2020-04-04 15:33:46 +0300

    01_sort: macro in main

--- 01_sort/main.cpp
+++ 01_sort/main.cpp
@@ -21,6 +21,17 @@ void output(Iter b, Iter e)
     std::cout << std::endl;
 }
 
+#define SORT(s, sort, sort_uni) \
+    std::cout << "== "#s" ==" << std::endl; \
+    to_sort = arr; \
+    (sort)(to_sort); \
+    std::cout << "vec: "; \
+    output(to_sort.begin(), to_sort.end()); \
+    to_sort = arr; \
+    (sort_uni)(to_sort.begin(), to_sort.end(), cmp<int>); \
+    std::cout << "uni: "; \
+    output(to_sort.begin(), to_sort.end());
+
 int main(int argc, char **argv)
 {
     int n = 0;
@@ -39,20 +50,10 @@ int main(int argc, char **argv)
     std::cout << std::endl << "Source:\t";
     output(arr.begin(), arr.end());
 
-    std::cout << "Insert:\t";
-    to_sort = arr;
-    sort_insert_uni(to_sort.begin(), to_sort.end(), cmp<int>);
-    output(to_sort.begin(), to_sort.end());
-
-    std::cout << "Shell:\t";
-    to_sort = arr;
-    sort_shell_uni(to_sort.begin(), to_sort.end(), cmp<int>);
-    output(to_sort.begin(), to_sort.end());
-
-    std::cout << "Bubble:\t";
-    to_sort = arr;
-    sort_bubble_uni(to_sort.begin(), to_sort.end(), cmp<int>);
-    output(to_sort.begin(), to_sort.end());
+    SORT(Insert, sort_insert, sort_insert_uni)
+    SORT(Shell, sort_shell, sort_shell_uni)
+    SORT(Bubble, sort_bubble, sort_bubble_uni)
+    SORT(Shaker, sort_shaker, sort_shaker_uni)
 
     return 0;
 }
